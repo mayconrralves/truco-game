b2510a461b9f90b87c46db5e6f0378a00a5d13cb
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-05-18 10:43:36 -0300

    width has been increased

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -6,14 +6,14 @@ export const StyledHeader= styled.header`
     align-items: center;
     width: 100%;
     border-bottom: 1px solid rgba(0,0,0, 0.2);
+    padding: 12px 0;
     div{
         display: flex;
         justify-content: space-between;
         align-items: baseline;
         h1 {
-            
-            width: 500px;
-            font-size: 3em;
+            width: 350px;
+            font-size: 2.5em;
             text-align: center;
         }
     }
